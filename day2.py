bc49897eb063ff7d45e9fff0645a28de0efb293f
Victoria  Topham <topham.victoria@gmail.com>
2020-07-28 14:39:40 -0700

    stack queue

--- day2.py
+++ day2.py
@@ -40,3 +40,58 @@ class Solution(object):
             
         return output
             
+
+#stack queue
+
+class MyQueue(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.storage = []
+        
+        
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: None
+        
+        """
+        self.storage.append(x)
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        return self.storage.pop(0)
+        
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        return self.storage[0]
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        if len(self.storage) > 0:
+            return False
+        else:
+            return True
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
